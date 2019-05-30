cba0ef75f9c5719629b746b7faa1a136aa9e9e69
SunWenyang <573534939@qq.com>
2019-05-30 10:26:02 +1000

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fsyd4-1.fna.fbcdn.net/vp/6c67bd85c1cfaf3946aac52f0d3354f1/5D655738/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fsyd4-1.fna.fbcdn.net" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong>  posts</div>
+                <div class="pr-5"><strong>23k</strong>  followers</div>
+                <div class="pr-5"><strong>212</strong>  following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
+            </div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    
+    <div class="row pt-5">
+        <div class="col-4"><img style="height:240px;" class="w-100" src="https://cdn.pixabay.com/photo/2015/07/09/22/45/tree-838667__340.jpg"></div>
+        <div class="col-4"><img style="height:240px;" class="w-100"  src="https://cdn.pixabay.com/photo/2016/11/06/05/36/landscape-1802337__340.jpg"></div>
+        <div class="col-4"><img style="height:240px;" class="w-100" src="https://cdn.pixabay.com/photo/2015/10/18/16/55/woman-994737__340.jpg" class="w-100"></div>  
+    </div>
+</div>
+@endsection
